06c2ea31809b2422671a935fec94343d6f9a8fe9
squizzling <squizzling@users.noreply.github.com>
2020-12-11 00:34:34 +1100

    Test to validate no non-determinism in StringTabelBuilder

--- internal/t/string_table_builder_test.go
+++ internal/t/string_table_builder_test.go
@@ -89,3 +89,31 @@ func TestStringTableBuilderPanics(t *testing.T) {
 		stb.GetIndex("def")
 	}()
 }
+
+func TestStringTableBuilderToKeyVals(t *testing.T) {
+	stb := NewStringTableBuilder()
+	stb.AddString("c")
+	stb.AddString("d")
+	stb.AddString("a")
+	stb.AddString("b")
+	stb.AddString("2")
+	stb.AddString("4")
+	stb.AddString("1")
+	stb.AddString("3")
+	stb.Finalize()
+	ks, vs := stb.ToKeyVals(map[string]string{"a": "1", "b": "2", "c": "3", "d": "4"})
+	// expected sort ordering is 1234abcd, as the keys are abcd they
+	// should be 5, 6, 7, 8 and the values should be 1, 2, 3, 4.
+	expectedKeys := []uint32{5, 6, 7, 8}
+	expectedVals := []uint32{1, 2, 3, 4}
+	for i := 0; i < 4; i++ {
+		if expectedKeys[i] != ks[i] {
+			t.Fatalf("keys is not as expected: %#v", ks)
+		}
+	}
+	for i := 0; i < 4; i++ {
+		if expectedVals[i] != vs[i] {
+			t.Fatalf("vals is not as expected: %#v", vs)
+		}
+	}
+}
